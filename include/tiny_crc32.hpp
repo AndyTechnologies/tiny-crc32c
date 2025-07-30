6f2aed746c1229b884071c1e782b567458449c5e
AndyTechnologies <gardyfon2403@gmail.com>
2025-07-30 00:04:32 -0400

    Fix en el _internals namespace

--- include/tiny_crc32.hpp
+++ include/tiny_crc32.hpp
@@ -4,28 +4,28 @@
 namespace tcrc32{
 
 	namespace _internals{
-		inline constexpr uint32_t __crc32_impl(
+		inline constexpr std::uint32_t __crc32_impl(
 			const uint8_t* data, 
 			std::size_t length, 
-			uint32_t init = 0
+			std::uint32_t init = 0
 		);
 	}
 
-inline constexpr uint32_t crc32(const uint8_t* data, std::size_t length, uint32_t init = 0);
+inline constexpr std::uint32_t crc32(const uint8_t* data, std::size_t length, std::uint32_t init = 0);
 
 // Overload for container with data() and size()
 template <typename Container>
 concept STLContainer = requires(Container a) { a.data(); a.size(); };
 
 template<STLContainer Container>
-inline constexpr uint32_t crc32(const Container& c, uint32_t init = 0) {
+inline constexpr std::uint32_t crc32(const Container& c, std::uint32_t init = 0) {
 	return crc32(reinterpret_cast<const uint8_t*>(c.data()), c.size(), init);
 }
 
 // Incremental CRC32C
 class CRC32C {
 public:
-    constexpr CRC32C(uint32_t init = 0);
+    constexpr CRC32C(std::uint32_t init = 0);
 
     inline constexpr void update(const uint8_t* data, std::size_t length);
 
@@ -34,10 +34,10 @@ public:
         update(reinterpret_cast<const uint8_t*>(c.data()), c.size());
     }
 
-    [[nodiscard]] inline constexpr uint32_t digest() const noexcept;
+    [[nodiscard]] inline constexpr std::uint32_t digest() const noexcept;
 
 private:
-    uint32_t crc_;
+    std::uint32_t crc_;
 };
 
 }
@@ -48,13 +48,13 @@ private:
 #include <array>
 
 namespace tcrc32{
-	namespace _inernals{
+	namespace _internals{
 		static constexpr const std::uint32_t POLY{  0x82f63b78u };
 
-		static consteval std::array<uint32_t, 256> compute() {
-			std::array<uint32_t, 256> table{};
-			for (uint32_t i = 0; i < 256; ++i) {
-				uint32_t crc = i;
+		static consteval std::array<std::uint32_t, 256> compute() {
+			std::array<std::uint32_t, 256> table{};
+			for (std::uint32_t i = 0; i < 256; ++i) {
+				std::uint32_t crc = i;
 				for (int j = 0; j < 8; ++j) {
 					if (crc & 1u)
 						crc = (crc >> 1) ^ POLY;
@@ -67,12 +67,12 @@ namespace tcrc32{
 		}
 		static constexpr auto CRC_TABLE{ compute() };
 
-		inline constexpr uint32_t __crc32_impl(
+		inline constexpr std::uint32_t __crc32_impl(
 			const uint8_t* data, 
 			std::size_t length, 
-			uint32_t init
+			std::uint32_t init
 		) {
-			uint32_t crc = ~init;
+			std::uint32_t crc = ~init;
 			for (std::size_t i = 0; i < length; ++i) {
 				uint8_t byte = data[i];
 				crc = CRC_TABLE[(crc ^ byte) & 0xFFu] ^ (crc >> 8);
@@ -83,21 +83,21 @@ namespace tcrc32{
 
 	}
 
-	inline constexpr uint32_t crc32(
+	inline constexpr std::uint32_t crc32(
 		const uint8_t* data, 
 		std::size_t length, 
-		uint32_t init
-	){ return _inernals::__crc32_impl(data, length, init); }
+		std::uint32_t init
+	){ return _internals::__crc32_impl(data, length, init); }
 
-	constexpr CRC32C::CRC32C(uint32_t init): crc_(~init){}
+	constexpr CRC32C::CRC32C(std::uint32_t init): crc_(~init){}
 	
 	inline constexpr void CRC32C::update(const uint8_t* data, std::size_t length){
 		for (std::size_t i = 0; i < length; ++i) {
-            crc_ = _inernals::CRC_TABLE[(crc_ ^ data[i]) & 0xFFu] ^ (crc_ >> 8);
+            crc_ = _internals::CRC_TABLE[(crc_ ^ data[i]) & 0xFFu] ^ (crc_ >> 8);
         }
 	}
 
-	inline constexpr uint32_t CRC32C::digest() const noexcept { return ~crc_; }
+	inline constexpr std::uint32_t CRC32C::digest() const noexcept { return ~crc_; }
 
 
 }
